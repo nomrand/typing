cf4cbc4e50451fec8dcfe83a4a1a72ad68641038
nomrand <42882840+nomrand@users.noreply.github.com>
2019-05-28 21:03:54 +0700

    ver 0.6

--- js/data.js
+++ js/data.js
@@ -1,8 +1,34 @@
-let WordMax = 10;
 let WordData = [
-    "abcdefghijklmn",
-    "1234567890",
-    "สวัสดีครับ",
-    "ไม่เป็นไร",
-    "ขอบคุณครับ",
+    {
+        mode: "easy",
+        speed: 20000,
+        max: 10,
+        data: [
+            "abcefg",
+            "jklmnop",
+            "1234567890",
+        ],
+    },
+    {
+        mode: "normal",
+        speed: 15000,
+        max: 10,
+        data: [
+            "abcdefghijklmnopqrstuvwxyz",
+            "1234567890",
+            "!#<>();:=",
+        ],
+    },
+    {
+        mode: "hard",
+        speed: 10000,
+        max: 20,
+        data: [
+            "abcdefghijklmn",
+            "1234567890",
+            "สวัสดีครับ",
+            "ไม่เป็นไร",
+            "ขอบคุณครับ",
+        ],
+    },
 ];
